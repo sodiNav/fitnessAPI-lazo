d5b07e21ab9d9d7e5f00e1d7f68297a23d336caf
jamesivnlazo <jamesivnlazo@gmail.com>
2026-06-16 05:42:46 +0800

    Fixed minor issue

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,12 @@ const express = require(`express`);
 const router = express.Router();
 const userController = require(`../controllers/user`);
 
+const { verify } = require('../auth');
+
 router.post("/register", userController.registerUser);
 
 router.post("/login", userController.loginUser);
 
-router.get("/details", userController.getProfile);
+router.get("/details", verify, userController.getProfile);
 
 module.exports = router;
